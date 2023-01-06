894a44b3c35a602b1054c63ff17f8a12bf19cc53
jang-taehyun <jth0202@naver.com>
2023-01-07 03:46:16 +0900

    OpenGL Lecture 2

--- OpenGLLecture/main.cpp
+++ OpenGLLecture/main.cpp
@@ -1,80 +1,3 @@
-/*
-# 참고 사이트
-OpenGL tutorial : https://www.opengl-tutorial.org/kr/
-OpenGL API : https://docs.gl/
-유튜브 참고 사이트 : https://www.youtube.com/TheChernoProject/video
-이론에 관한 사이트 : https://www.songho.ca/opengl/index.html
-
-# 코드 참고 github 사이트 : https://github.com/diskhkme/OpenGL_Lecture_Material
-- commit을 눌러서 차근차근 보자
-- '+' 부분 : 추가된 부분
-  '-' 부분 : 제거된 부분
-
-# function의 parameter와 용도는 OpenGL API를 참고
-# 코드의 흐름을 파악하는게 우선 중요
-*/
-
-/*
-* 이론
-3D 컴퓨터 그래픽스 : obejct를 3D로 표현
-
-rendering : 이미지를 만들어내는 과정
-rendering 과정 : geometry(형상) -> texture(색상) -> lighting(빛의 방향 및 세기)
-
-렌더링 필요한 data : 빛, 카메라, 물체
--> 이 data들을 조합해 픽셀의 color data를 계산한다.
-
-graphics pipeline를 병렬적으로 처리한다.
-vertex data, primitive data -> tessellation -> vertex processing -> geometry processing -> pixel processing -> pixel rendering
-											   textrured surface ->	texture sampler ----->
-
-영화, 애니메이션 -> 비실시간 컴퓨터 그래픽스(30fps 이하)
-게임 -> 실시간 컴퓨터 그래픽스(30fps 이상)
-
-컴퓨터 그래픽스 주요 단계 : modeling -> rigging -> animation -> rendering -> post-processing
--> 필요한 자원 : modeling, rigging, animation(art 영역)
--> run-time에 하는 동작 : rendering, post-processing(개발 영역)
-
-- modeling : 컴퓨터가 물체를 표현한 것(geometry, 형상)
-  -> polygon mesh 사용 : polygon mesh의 집합을 사용
-     -> polygon mesh 중 triangle 사용
-  -> texture 사용 : model의 색상(입혀진 것)을 입히는데 사용
-     -> texture는 이미지 data로 저장한다.
-
-- rigging(skeleton) : model에 뼈를 정의하는 과정
-  -> polygon mesh와 함께 rigging 작업을 한다.
-
-- animation : 특정 event 발생 시, 발생하는 animation
-  -> keyframe animation을 사용한다.(Quaternion)
-
-- rendering : 3D scene(3D data)을 이욯해 2D imgae로 만드는 과정
-- post-processing(후처리) : 1 frame에 처리할 수 없는 것을 처리하는 과정, rendering에 처리할 수 없는 것을 처리
-  ex) motion blur(움직이고 있는 물체를 흐리게 표현하는 것)
-
-# Graphics API : GPU 안에 일어나는 instruction을 호출하는 것
-
-
-* 실기
-GLFW : window와 관련된 처리를 쉽게 사용할 수 있도록 구현한 library
--> WinAPI를 사용할 수 있지만, 시간이 오래 걸리기 때문에 사용
--> window's context : OpenGL의 결과물을 그릴 window
-
-GLEW : OpenGL의 최신 버전을 사용하기 위해 필요한 library
-
-State-Machine 방식 : 현재 activate된 것만 그리는 방식
-- 과정 : object 정의 -> object를 activate 상태로 설정 -> draw function 호출
-- bind : buffer를 activate 상태로 설정하는 것
-
-접두어가 glfw : glfw library에 있는 function
-접두어가 gl   : OpenGL API에 있는 function
--> 렌더링과 관련된 function은 대부분 gl로 시작한다.
-
-Double-buffering 사용 : back buffer를 먼저 그리고 front buffer와 swap하는 방식
-
-glBegin() 또는 glEnd() function이 존재하는 코드는 오래된 version이다.(Legacy 코드)
--> shader가 없을 떄 알아서 흰색으로 그리도록 하는 function
-*/
-
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
@@ -82,6 +5,65 @@ glBegin() 또는 glEnd() function이 존재하는 코드는 오래된 version이다.(Legacy 코�
 // window size setting
 const int WIN_WIDTH = 800;
 const int WIN_HEIGHT = 600;
+
+// shader compile function
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    // shader object 생성
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+
+    // shader source code 해석 방법 & shader source code를 GPU에 전달
+    glShaderSource(id,          // complie된 shader의 id를 저장할 위치
+                   1,           // compile할 source code의 개수
+                   &src,        // 실제 source code가 있는 문자열 주소값
+                   nullptr);    // 해당 문자열 전체를 사용(nullptr), 아니라면 길이 명시
+
+    // shader source code 컴파일
+    glCompileShader(id);
+
+    // Error Handling
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result); //셰이더 프로그램으로부터 컴파일 결과(log)를 얻어옴
+    if (result == GL_FALSE) //컴파일에 실패한 경우
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length); //log의 길이를 얻어옴
+        char* message = (char*)alloca(length * sizeof(char)); //stack에 동적할당
+        glGetShaderInfoLog(id, length, &length, message); //길이만큼 log를 얻어옴
+        std::cout << "셰이더 컴파일 실패! " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id); //컴파일 실패한 경우 셰이더 삭제
+        return 0;
+    }
+
+    // shader id return
+    return id;
+}
+
+// shader program 생성 function
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragShader)
+{
+    // shader program object 생성 및 shader complie
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragShader);
+
+    // compile된 shader를 shader program에 추가 및 링크
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+
+    // shader program이 현재 state에서 동작 가능한지 확인
+    glValidateProgram(program);
+
+    // shader 반환
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    // shader program id return
+    return program;
+}
 
 int main()
 {
@@ -116,12 +98,12 @@ int main()
     // OpenGL version 출력
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    // 삼각형 정의
+    // 삼각형 정의(현재 CPU side memory에 존재)
     float position[] =
     {
-        -0.5f, -0.5f,
-         0.f,   0.5f,
-         0.5f, -0.5f
+        -0.5f, -0.5f, 0.f,
+         0.f,   0.5f, 0.f,
+         0.5f, -0.5f, 0.f
     };
 
     // buffer 생성
@@ -129,8 +111,50 @@ int main()
     glGenBuffers(1, &bufferID);                     // buffer 생성
     glBindBuffer(GL_ARRAY_BUFFER, bufferID);        // 생성한 buffer의 종류 명시 & bind
     
-    // buffer에 삼각형 data 전달
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), position, GL_STATIC_DRAW);
+    // buffer에 저장된 삼각형 data을 GPU에 전달
+    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), position, GL_STATIC_DRAW);
+
+    // -------------- buffer에 저장된 data 해석 방법 명시 --------------- //
+    // glEnableVertexAttribArray의 parameter와 glVertexAttribPointer의 parameter 1의 의미 : vertex shader에 저장할 location
+
+    // data 저장 위치 설정
+    glEnableVertexAttribArray(0);                   // vertex shader의 0번 location에 저장
+
+    // data를 어떻게 나눠서 해석할지 설정
+    glVertexAttribPointer(0,                        // 해석할 data가 저장된 location(glEnableVertexAttribArray에서 설정한 location)
+                          3,                        // 하나의 vertex에 몇개의 data를 넘기는지(vertex data 중 넘길 data의 개수), 여기에선 위치정보(xyz 좌표)만 있으므로 3
+                          GL_FLOAT,                 // data의 자료형
+                          GL_FALSE,                 // normalization 유무
+                          sizeof(float) * 3,        // stride : vertex 하나의 크기(byte 단위)
+                          0);                       // offset : 시작할 index 번호
+
+    // -------------- shader 생성 및 compile --------------- //
+    // GLSL 언어로 shader source code 작성
+    std::string vertexShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;\n"      // 여기 있는 location = 0가, glEnableVertexAttribArray의 parameter와 glVertexAttribPointer의 parameter 1의 의미함
+                                                        // input이 들어오기 때문에 in 키워드 사용 & position 부분은 아무 이름으로 해도 괜찮음
+        "\n"
+        "void main()\n"
+        "{\n"
+        "    gl_Position = position;\n"                 // 3개의 값만 전달했지만, 알아서 vec4로 변환
+        "}";
+    std::string fragShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;\n"        // 출력 color 
+        "\n"
+        "void main()\n"
+        "{\n"
+        "    color = vec4(1.f, 1.f, 0.f, 1.f);\n"       // 노란색 변환(각 vertex마다 어떤 색을 출력할지 결정)
+        "}";
+
+    // shader program 생성 및 shader program object ID 저장
+    unsigned int shader = CreateShader(vertexShader, fragShader);
+
+    // shader program bind(shader program을 activate 상태로 전환)
+    glUseProgram(shader);      
 
     // Rendering loop
     while (!glfwWindowShouldClose(window))
@@ -148,9 +172,10 @@ int main()
         glfwPollEvents();
     }
 
-    // resource 반환
+    // shader program 반환
+    glDeleteProgram(shader);
+
+    // GLFW resource 반환
     glfwTerminate();
     return 0;
 }
-
-// 수정 test임돠
